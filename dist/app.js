44c17f829bf9578e97e481d9dec011d4a360df2b
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-11-12 17:43:58 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./public'));
+app.listen(5000, () => console.log('Server started listening...'));
